de978e59accabc53d07a364b1c60431c1c42c8c4
BillyWu423 <zihengwubilly@outlook.com>
2025-06-24 16:25:34 -0400

    Implement multi-module REST + MyBatis structure

---
+++ bilibili-service/src/main/java/com/example/bilibili/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.example.bilibili.service;
+
+import com.example.bilibili.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    @Autowired
+    private DemoDao demoDao;
+
+    public Long query(Long id) {
+        return demoDao.query(id);
+    }
+
+
+}
