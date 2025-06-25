c6a248d9f231f1728a7e760adcef68cb50df5ff6
BillyWu423 <zihengwubilly@outlook.com>
2025-06-25 16:49:34 -0400

    Configure DevTools & MyBatis; implement map-based Demo API; add commons-codec utils

--- bilibili-dao/src/main/java/com/example/bilibili/dao/DemoDao.java
+++ bilibili-dao/src/main/java/com/example/bilibili/dao/DemoDao.java
@@ -2,8 +2,11 @@ package com.example.bilibili.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface DemoDao {
-    public Long query(Long id);
+
+    public Map<String, Object> query(Long id);
 
 }
